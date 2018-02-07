cbc755fee15a5ba8477d4af35d169960725ba507
Brian Levasseur <blevasseur@kayak.com>
2018-02-07 17:36:16 +0100

    "Add stock quote support"

---
+++ src/main/java/org/poker/listener/StockQuoteMessageListener.java
@@ -0,0 +1,103 @@
+package org.poker.listener;
+
+import com.ullink.slack.simpleslackapi.SlackAttachment;
+import com.ullink.slack.simpleslackapi.SlackChannel;
+import com.ullink.slack.simpleslackapi.SlackSession;
+import com.ullink.slack.simpleslackapi.SlackUser;
+import com.ullink.slack.simpleslackapi.events.SlackMessagePosted;
+import com.ullink.slack.simpleslackapi.listeners.SlackMessagePostedListener;
+import humanize.ICUHumanize;
+import org.knowm.xchange.Exchange;
+import org.knowm.xchange.ExchangeFactory;
+import org.knowm.xchange.ExchangeSpecification;
+import org.knowm.xchange.binance.BinanceExchange;
+import org.knowm.xchange.binance.dto.marketdata.BinanceTicker24h;
+import org.knowm.xchange.binance.service.BinanceMarketDataService;
+import org.knowm.xchange.binance.service.BinanceMarketDataServiceRaw;
+import org.knowm.xchange.coinmarketcap.CoinMarketCapExchange;
+import org.knowm.xchange.coinmarketcap.dto.marketdata.CoinMarketCapTicker;
+import org.knowm.xchange.coinmarketcap.service.CoinMarketCapMarketDataService;
+import org.knowm.xchange.currency.CurrencyPair;
+import org.knowm.xchange.gdax.GDAXExchange;
+import org.knowm.xchange.gdax.dto.marketdata.GDAXProductTicker;
+import org.knowm.xchange.gdax.service.GDAXMarketDataService;
+import yahoofinance.Stock;
+import yahoofinance.YahooFinance;
+import yahoofinance.quotes.stock.StockQuote;
+
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.math.MathContext;
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class StockQuoteMessageListener implements SlackMessagePostedListener {
+  private static final DecimalFormat decimalFormat = new DecimalFormat("#,###.00");;
+
+  public StockQuoteMessageListener() {
+  }
+
+  public void onEvent(SlackMessagePosted event, SlackSession session) {
+    String message = event.getMessageContent();
+    if (!message.startsWith(".") || message.length() > 10) {
+      return;
+    }
+    String coin = getCurrencyCodeFromMessage(message);
+    SlackChannel channel = event.getChannel();
+
+    try {
+      Stock stock = YahooFinance.get(coin);
+      if (stock.isValid()) {
+        StockQuote quote = stock.getQuote();
+        SlackAttachment attachment = formatAttachment(stock, quote);
+        session.sendMessage(channel, attachment.getFallback(), attachment);
+      }
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  private String getCurrencyCodeFromMessage(String message) {
+    return message.substring(1);
+  }
+
+  private SlackAttachment formatAttachment(Stock stock, StockQuote quote) {
+    SlackAttachment attachment = new SlackAttachment();
+    attachment.setFallback("");
+    attachment.setColor(getColor(quote.getChange()));
+    //TODO: find better place for images to support moar exchanges
+    attachment.setThumbUrl("https://www.nasdaq.com/logos/" + stock.getSymbol().toUpperCase() + ".GIF");
+    attachment.addField(stock.getName(), formatMessage(stock, quote), false);
+    return attachment;
+  }
+
+  private String formatMessage(Stock stock, StockQuote quote) {
+    BigDecimal usdPrice = quote.getPrice();
+    BigDecimal percentChange24Hour = quote.getChangeInPercent();
+    boolean isZeroOrPositive = percentChange24Hour.compareTo(BigDecimal.ZERO) >= 0;
+    String strMessage = String.format("$%s %s%s (%s%%) | Cap: %s | Vol: %s",
+            decimalFormat.format(usdPrice),
+            isZeroOrPositive ? "+" : "",
+            decimalFormat.format(quote.getChange()),
+            decimalFormat.format(percentChange24Hour),
+            "$" + ICUHumanize.compactDecimal(stock.getStats().getMarketCap()),
+            ICUHumanize.compactDecimal(quote.getVolume()));
+    return strMessage;
+  }
+
+  private String getColor(BigDecimal percentChange24Hour) {
+    int cmp = percentChange24Hour.compareTo(BigDecimal.ZERO);
+    if (cmp == 0) {
+      return "warning";
+    } else if (cmp > 0) {
+      return "good";
+    } else {
+      if (percentChange24Hour.compareTo(new BigDecimal(-10, MathContext.DECIMAL32)) >= 0) {
+        return "warning";
+      }
+      return "danger";
+    }
+  }
+}
