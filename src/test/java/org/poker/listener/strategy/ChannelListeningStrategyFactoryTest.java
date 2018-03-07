91d777923c89d1292795344a7413ddac0ab696d1
Rob Leidle <spew@github.com>
2018-03-07 10:58:02 -0800

    More main run loop simplification and a shutdown hook to atttempt to disconnect.

---
+++ src/test/java/org/poker/listener/strategy/ChannelListeningStrategyFactoryTest.java
@@ -0,0 +1,24 @@
+package org.poker.listener.strategy;
+
+import org.junit.Test;
+import org.poker.Stage;
+
+import static org.hamcrest.CoreMatchers.instanceOf;
+import static org.junit.Assert.assertThat;
+
+public class ChannelListeningStrategyFactoryTest {
+    @Test
+    public void production() {
+        assertStageAndClass(Stage.Production, OnlyListenToSpecificChannelsStrategy.class);
+    }
+
+    @Test
+    public void gamma() {
+        assertStageAndClass(Stage.Gamma, IgnoreChannelsStrategy.class);
+    }
+
+    private void assertStageAndClass(Stage stage, Class klass) {
+        ChannelListeningStrategy strategy = ChannelListeningStrategyFactory.newDefault(stage);
+        assertThat(strategy, instanceOf(klass));
+    }
+}
