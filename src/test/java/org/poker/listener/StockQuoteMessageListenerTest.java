b283be98272c81ff65655b8a238a18ee05dac832
Rob Leidle <rleidle@google.com>
2018-06-21 17:38:53 -0700

    Improve stock quotes so they are on a single line.

--- src/test/java/org/poker/listener/StockQuoteMessageListenerTest.java
+++ src/test/java/org/poker/listener/StockQuoteMessageListenerTest.java
@@ -1,76 +1,70 @@
 package org.poker.listener;
 
 import com.ullink.slack.simpleslackapi.SlackAttachment;
+import com.ullink.slack.simpleslackapi.SlackPreparedMessage;
 import com.ullink.slack.simpleslackapi.SlackSession;
 import com.ullink.slack.simpleslackapi.events.SlackMessagePosted;
 import org.junit.Test;
 import org.mockito.ArgumentCaptor;
 import org.poker.stock.YahooFinanceStockResolver;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 import static org.mockito.Mockito.*;
 
 public class StockQuoteMessageListenerTest {
     @Test
     public void amznTicker() {
-        StockQuoteMessageListener listener = new StockQuoteMessageListener(new YahooFinanceStockResolver());
-        SlackMessagePosted event = mock(SlackMessagePosted.class);
-        when(event.getMessageContent()).thenReturn("$amzn");
-        SlackSession session = mock(SlackSession.class);
-        listener.onEvent(event, session);
-        ArgumentCaptor<SlackAttachment> argumentCaptor = ArgumentCaptor.forClass(SlackAttachment.class);
-        verify(session, times(1)).sendMessage(any(), any(String.class), argumentCaptor.capture());
-        SlackAttachment actualAttachment = argumentCaptor.getValue();
-        assertEquals(1, actualAttachment.getFields().size());
-        assertEquals("Amazon.com, Inc.", actualAttachment.getFields().get(0).getTitle());
+        assertTickers("$amzn", Arrays.asList("Amazon.com, Inc."));
     }
 
     @Test
     public void brkTicker() {
-        StockQuoteMessageListener listener = new StockQuoteMessageListener(new YahooFinanceStockResolver());
-        SlackMessagePosted event = mock(SlackMessagePosted.class);
-        when(event.getMessageContent()).thenReturn("$brk.a");
-        SlackSession session = mock(SlackSession.class);
-        listener.onEvent(event, session);
-        ArgumentCaptor<SlackAttachment> argumentCaptor = ArgumentCaptor.forClass(SlackAttachment.class);
-        verify(session, times(1)).sendMessage(any(), any(String.class), argumentCaptor.capture());
-        SlackAttachment actualAttachment = argumentCaptor.getValue();
-        assertEquals(1, actualAttachment.getFields().size());
-        assertEquals("Berkshire Hathaway Inc.", actualAttachment.getFields().get(0).getTitle());
+        assertTickers("$brk.a", Arrays.asList("Berkshire Hathaway Inc."));
     }
 
     @Test
     public void djiTicker() {
-        StockQuoteMessageListener listener = new StockQuoteMessageListener(new YahooFinanceStockResolver());
-        SlackMessagePosted event = mock(SlackMessagePosted.class);
-        when(event.getMessageContent()).thenReturn("$^dji");
-        SlackSession session = mock(SlackSession.class);
-        listener.onEvent(event, session);
-        ArgumentCaptor<SlackAttachment> argumentCaptor = ArgumentCaptor.forClass(SlackAttachment.class);
-        verify(session, times(1)).sendMessage(any(), any(String.class), argumentCaptor.capture());
-        SlackAttachment actualAttachment = argumentCaptor.getValue();
-        assertEquals(1, actualAttachment.getFields().size());
-        assertEquals("Dow Jones Industrial Average", actualAttachment.getFields().get(0).getTitle());
+        assertTickers("$^dji", Arrays.asList("Dow Jones Industrial Average"));
     }
 
     @Test
     public void multipleTickers() {
-        StockQuoteMessageListener listener = new StockQuoteMessageListener(new YahooFinanceStockResolver());
-        SlackMessagePosted event = mock(SlackMessagePosted.class);
-        when(event.getMessageContent()).thenReturn("$amzn $goog");
-        SlackSession session = mock(SlackSession.class);
-        listener.onEvent(event, session);
-        ArgumentCaptor<SlackAttachment> argumentCaptor = ArgumentCaptor.forClass(SlackAttachment.class);
-        verify(session, times(2)).sendMessage(any(), any(String.class), argumentCaptor.capture());
-        assertEquals(2, argumentCaptor.getAllValues().size());
-        SlackAttachment actualAttachment = argumentCaptor.getAllValues().get(0);
-        assertEquals(1, actualAttachment.getFields().size());
-        assertTickerAttachment(argumentCaptor.getAllValues().get((0)), "Amazon.com, Inc.");
-        assertTickerAttachment(argumentCaptor.getAllValues().get((1)), "Alphabet Inc.");
+        assertTickers("$amzn $goog", Arrays.asList("Amazon.com, Inc.", "Alphabet Inc."));
     }
 
-    private void assertTickerAttachment(SlackAttachment attachment, String title) {
-        assertEquals(1, attachment.getFields().size());
-        assertEquals(title, attachment.getFields().get(0).getTitle());
+    private void assertTickers(String message, List<String> expectedTitles) {
+        StockQuoteMessageListener listener = new StockQuoteMessageListener(new YahooFinanceStockResolver());
+        SlackMessagePosted event = mock(SlackMessagePosted.class);
+        when(event.getMessageContent()).thenReturn(message);
+        SlackSession session = mock(SlackSession.class);
+        listener.onEvent(event, session);
+        ArgumentCaptor<SlackPreparedMessage> argumentCaptor = ArgumentCaptor.forClass(SlackPreparedMessage.class);
+        verify(session, times(1)).sendMessage(any(), argumentCaptor.capture());
+        SlackPreparedMessage actualMessage = argumentCaptor.getValue();
+        List<SlackAttachment> attachments = new ArrayList<>();
+        for (String t : expectedTitles) {
+            SlackAttachment expected = new SlackAttachment();
+            expected.addField(t, "", false);
+            attachments.add(expected);
+        }
+        assertSlackPreparedMessage(attachments, actualMessage);
+    }
+
+    private void assertSlackPreparedMessage(List<SlackAttachment> expectedAttachments, SlackPreparedMessage actualMessage) {
+        assertNotNull(actualMessage.getAttachments());
+        assertEquals(actualMessage.getAttachments().length, expectedAttachments.size());
+        for (int i = 0; i < actualMessage.getAttachments().length; i++) {
+            assertTickerAttachment(expectedAttachments.get(i), actualMessage.getAttachments()[i]);
+        }
+    }
+
+    private void assertTickerAttachment(SlackAttachment expected, SlackAttachment actual) {
+        assertEquals(1, actual.getFields().size());
+        assertEquals(expected.getFields().get(0).getTitle(), actual.getFields().get(0).getTitle());
     }
 }
