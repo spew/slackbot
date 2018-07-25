0794bc1e8e7e7cddc8361b8e7cece60e7b4cc3f3
Brian Levasseur <blevasseur@kayak.com>
2018-07-25 15:53:41 +0200

    Add dastocks to production channel whitelist

--- src/main/java/org/poker/config/Channel.java
+++ src/main/java/org/poker/config/Channel.java
@@ -6,7 +6,8 @@ import java.util.List;
 public class Channel {
     public static final String MAIN_CHANNEL = "general";
     public static final String COIN_CHANNEL = "dacoins";
-    public static final List<String> PRODUCTION_CHANNELS = Arrays.asList(MAIN_CHANNEL, COIN_CHANNEL);
+    public static final String STOCK_CHANNEL = "dastocks";
+    public static final List<String> PRODUCTION_CHANNELS = Arrays.asList(MAIN_CHANNEL, COIN_CHANNEL, STOCK_CHANNEL);
 
     public static final String BOT_CHANNEL = "bot";
     public static final List<String> GAMMA_CHANNELS = Arrays.asList(BOT_CHANNEL);
