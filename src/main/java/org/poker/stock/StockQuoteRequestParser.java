8f9bb3cf97422251f0cb0fee7ddff4de0c07e890
Rob Leidle <rleidle@google.com>
2018-07-26 09:00:26 -0700

    Add FAANG ticker for stocks

--- src/main/java/org/poker/stock/StockQuoteRequestParser.java
+++ src/main/java/org/poker/stock/StockQuoteRequestParser.java
@@ -1,17 +1,31 @@
 package org.poker.stock;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class StockQuoteRequestParser {
+    private static final String FAANG_TICKER = "FAANG";
+    private static final List<String> FAANG_TICKER_LIST = Arrays.asList("FB", "AMZN", "AAPL", "NFLX", "GOOG");
+
     public List<String> getTickers(String message) {
         List<String> results = new ArrayList<>();
         String[] tokens = message.split(" ");
         for (String t : tokens) {
             if (t.startsWith("$") && t.length() <= 10) {
-                results.add(t.substring(1).trim());
+                String ticker = t.substring(1).trim();
+                results.addAll(expandTicker(ticker));
             }
         }
         return results;
+    }
+
+    private List<String> expandTicker(String ticker) {
+        switch (ticker) {
+            case FAANG_TICKER:
+                return FAANG_TICKER_LIST;
+            default:
+                return Arrays.asList(ticker);
+        }
     }
 }
