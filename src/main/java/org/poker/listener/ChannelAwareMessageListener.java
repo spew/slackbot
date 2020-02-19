160045dd44a47dec373d8dbb52b6171676651a27
Rob Leidle <rleidle@google.com>
2020-02-19 07:41:16 -0800

    Output a minimal error message when there is an exception

--- src/main/java/org/poker/listener/ChannelAwareMessageListener.java
+++ src/main/java/org/poker/listener/ChannelAwareMessageListener.java
@@ -3,6 +3,8 @@ package org.poker.listener;
 import com.ullink.slack.simpleslackapi.SlackSession;
 import com.ullink.slack.simpleslackapi.events.SlackMessagePosted;
 import com.ullink.slack.simpleslackapi.listeners.SlackMessagePostedListener;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.poker.listener.strategy.ChannelListeningStrategy;
 
 public class ChannelAwareMessageListener implements SlackMessagePostedListener {
@@ -17,7 +19,23 @@ public class ChannelAwareMessageListener implements SlackMessagePostedListener {
     @Override
     public void onEvent(SlackMessagePosted event, SlackSession session) {
         if (channelListeningStrategy.shouldHandleEvent(event, session)) {
-            listener.onEvent(event, session);
+            try {
+                listener.onEvent(event, session);
+            } catch (Exception e) {
+                session.sendMessage(event.getChannel(), formatExceptionMessage(e));
+            }
         }
+    }
+
+    private String formatExceptionMessage(Exception e) {
+        String listenerName = listener.getClass().getName();
+        String stackTrace = ExceptionUtils.getStackTrace(e);
+        int idx = StringUtils.ordinalIndexOf(stackTrace, "\n", 3);
+        if (idx > 0) {
+            stackTrace = stackTrace.substring(0, idx);
+        }
+        // indent the stacktrace
+        stackTrace = stackTrace.replaceAll("(?m)^", "  ");
+        return String.format("Unhandled exception in %s: %s\n%s", listenerName, e.getMessage(), stackTrace);
     }
 }
